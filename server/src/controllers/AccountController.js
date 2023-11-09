863377d8d248bd765a3df040e14ff646738cb9b9
code . <blazej686@yahoo.com>
2023-11-09 14:19:02 -0700

    Get favs

--- server/src/controllers/AccountController.js
+++ server/src/controllers/AccountController.js
@@ -1,11 +1,13 @@
 import { Auth0Provider } from '@bcwdev/auth0provider'
 import { accountService } from '../services/AccountService'
 import BaseController from '../utils/BaseController'
+import { favoritesService } from '../services/FavoritesService.js'
 
 export class AccountController extends BaseController {
   constructor() {
     super('account')
     this.router
+      .get(`/:accountId/favorites`, this.getFavoritesByAccountId)
       .use(Auth0Provider.getAuthorizedUserInfo)
       .get('', this.getUserAccount)
   }
@@ -17,5 +19,16 @@ export class AccountController extends BaseController {
     } catch (error) {
       next(error)
     }
+  }
+
+  async getFavoritesByAccountId(req, res, next) {
+    try {
+      const accountId = req.params.accountId
+      const favorites = await favoritesService.getFavoritesByAccountId(accountId)
+      res.send(favorites)
+    } catch (error) {
+      next(error)
+    }
+
   }
 }
--- server/src/controllers/FavoritesController.js
+++ server/src/controllers/FavoritesController.js
@@ -1,9 +1,16 @@
+import { Auth0Provider } from "@bcwdev/auth0provider";
 import BaseController from "../utils/BaseController.js";
+import { favoritesService } from "../services/FavoritesService.js";
 
 export class FavoritesController extends BaseController {
     constructor() {
         super('api/favorites')
         this.router
 
+            .use(Auth0Provider.getAuthorizedUserInfo)
+
     }
+
+
+
 }
