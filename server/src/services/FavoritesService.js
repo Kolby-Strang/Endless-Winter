1cad892ac9038376bb17942b79db4381988c0ebe
Kolby-Strang <137920922+Kolby-Strang@users.noreply.github.com>
2023-11-10 11:49:14 -0700

    Favorite delete method changed

--- server/src/services/FavoritesService.js
+++ server/src/services/FavoritesService.js
@@ -10,13 +10,17 @@ class FavoritesService {
         if (favorite.accountId != userId) {
             throw new UnAuthorized('You do not have permission to delete that favorite')
         }
-        favorite.delete()
+        await favorite.delete()
         return 'Favorite Deleted'
     }
     async createFavorite(favoriteData) {
+        const existingFavorite = await dbContext.Favorite.findOne({ snoId: favoriteData.snoId, accountId: favoriteData.accountId })
+        if (existingFavorite) {
+            const message = await this.destroyFavorite(existingFavorite.id, existingFavorite.accountId.toString())
+            return message
+        }
         const favorite = await dbContext.Favorite.create(favoriteData)
         return favorite
-
     }
     async getFavoritesByAccountId(accountId) {
 
