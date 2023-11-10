e9edecb569e79518e6d501d6cb1058e6df551b61
Kolby-Strang <137920922+Kolby-Strang@users.noreply.github.com>
2023-11-09 20:02:25 -0700

    Review - Put, Post - Model

--- server/src/controllers/ReviewsController.js
+++ server/src/controllers/ReviewsController.js
@@ -6,9 +6,21 @@ export class ReviewsController extends BaseController {
     constructor() {
         super('api/reviews')
         this.router
-
             .use(Auth0Provider.getAuthorizedUserInfo)
             .post('', this.createReview)
+            .put('/:reviewId', this.updateReview)
+    }
+    async updateReview(req, res, next) {
+        try {
+            const userId = req.userInfo.id
+            const reviewData = req.body
+            const reviewId = req.params.reviewId
+            reviewData.id = reviewId
+            const review = await reviewsService.updateReview(reviewData, userId)
+            res.send(review)
+        } catch (error) {
+            next(error)
+        }
     }
     async createReview(req, res, next) {
         try {
--- server/src/db/DbContext.js
+++ server/src/db/DbContext.js
@@ -4,6 +4,7 @@ import { ValueSchema } from '../models/Value'
 import { ResortSchema } from '../models/Resort.js';
 import { FavoriteSchema } from '../models/Favorite.js';
 import { ReviewSchema } from '../models/Review.js';
+import { PostSchema } from "../models/Post.js";
 
 class DbContext {
   Values = mongoose.model('Value', ValueSchema);
@@ -11,6 +12,7 @@ class DbContext {
   Resort = mongoose.model('Resort', ResortSchema)
   Favorite = mongoose.model('Favorite', FavoriteSchema)
   Review = mongoose.model('Review', ReviewSchema)
+  Post = mongoose.model('Post', PostSchema)
 }
 
 export const dbContext = new DbContext()
