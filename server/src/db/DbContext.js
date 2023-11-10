e0844c6f0cd756d8fb43c9b714427bfc8e5317dd
Kolby-Strang <137920922+Kolby-Strang@users.noreply.github.com>
2023-11-10 09:54:11 -0700

    Comments - Setup

--- server/src/db/DbContext.js
+++ server/src/db/DbContext.js
@@ -5,6 +5,7 @@ import { ResortSchema } from '../models/Resort.js';
 import { FavoriteSchema } from '../models/Favorite.js';
 import { ReviewSchema } from '../models/Review.js';
 import { PostSchema } from "../models/Post.js";
+import { CommentSchema } from '../models/Comment.js';
 
 class DbContext {
   Values = mongoose.model('Value', ValueSchema);
@@ -13,6 +14,7 @@ class DbContext {
   Favorite = mongoose.model('Favorite', FavoriteSchema)
   Review = mongoose.model('Review', ReviewSchema)
   Post = mongoose.model('Post', PostSchema)
+  Comment = mongoose.model('Comment', CommentSchema)
 }
 
 export const dbContext = new DbContext()
---
+++ server/src/models/Comment.js
@@ -0,0 +1,10 @@
+import { Schema } from "mongoose";
+
+export const CommentSchema = new Schema({
+    postId: { type: Schema.Types.ObjectId, required: true },
+    accountId: { type: Schema.Types.ObjectId, required: true },
+    body: { type: String, required: true, maxLength: 500 }
+}, {
+    timestamps: true,
+    toJSON: { virtuals: true }
+})
