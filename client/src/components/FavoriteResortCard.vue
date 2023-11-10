e9fd88044b92e868234bec1218c736752fc7b3dd
Liam Ball <davidliamb38@gmail.com>
2023-11-10 15:08:51 -0700

    wooooorkkkkinnngg ;;;;]]]

--- client/src/components/FavoriteResortCard.vue
+++ client/src/components/FavoriteResortCard.vue
@@ -5,10 +5,10 @@
       <div class=" p-2 ">
       <h3 class="mb-0">{{ resort.resortName }}</h3>
       <p class="mb-0">{{ resort.operatingStatus }}</p>
-      <span class="d-flex align-items-center" :title="resort.openDownHillLifts+'/'+resort.maxOpenDownHillLifts"><i class="mdi mdi-gondola fs-2 p-1"></i> <div class="bg-light bar rounded-pill"><p class="mb-0" :style="{width: resort.openDownHillLiftsPercentage+'%'}"></p></div></span>
+      <span class="d-flex align-items-center" :title="resort.openDownHillLifts+'/'+resort.maxOpenDownHillLifts"><i class="mdi mdi-gondola fs-2 p-1"></i> <div class="bar rounded-pill" :style="{background:'linear-gradient(90deg, rgba(39,36,84,1) '+resort.openDownHillLiftsPercentage+'%, rgba(255,255,255,1) '+resort.openDownHillLiftsPercentage+'%)'}"></div></span>
     </div>
     <p class="mb-0">{{ resort.snowComments }}</p>
-    <p class="mb-0">{{ resort.primarySurfaceCondition }}</p>
+    <p class="">{{ resort.primarySurfaceCondition }}</p>
   </div>
   <div class="col-4">
   </div>
