4ece9d84db11dd4aa9ad41970476ca50aa993d88
code . <blazej686@yahoo.com>
2023-11-13 11:42:00 -0700

    Added more resorts

---
+++ client/src/json/europe.js
@@ -0,0 +1,22 @@
+export const europe = [
+    {
+        "id": "222022",
+        "resortName": "St. Johann in Tirol",
+        "state": "AT",
+        "country": "AUT",
+        "region": "AUSTRIA",
+        "resortType": "Intl",
+        "package": "basic"
+    },
+
+    {
+        "id": "8000050",
+        "resortName": "Voss Resort Fjellheisar",
+        "state": "NO",
+        "country": "NOR",
+        "region": "NORWAY",
+        "resortType": "Intl",
+        "package": "basic"
+    }
+
+]
