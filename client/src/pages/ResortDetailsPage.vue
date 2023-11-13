8c8277e01c8f16dfd0b2ac52f10c0ba6e356364b
MarcS2 <marcellosharrai@hotmail.com>
2023-11-13 12:45:53 -0700

    details page slightly updated

--- client/src/pages/ResortDetailsPage.vue
+++ client/src/pages/ResortDetailsPage.vue
@@ -1,17 +1,17 @@
 <template>
-  <section class="row justify-content-center margin">
+  <section class="row justify-content-center margin ">
     <!-- SECTION resort details card -->
-    <div class="col-10">
-      <section class="row p-1 background-blur rounded">
+    <div class="col-10 background-blur rounded">
+      <section class="row p-1 ">
         <div class="col-6 text-light">
           <p class="fs-2 mb-0 d-inline">
             {{ resort.resortName }}
           </p> 
           <p class=" ms-2 d-inline">
-            Closed/Open
+            {{ resort.operatingStatus }}
           </p>
           <p class="mb-0">
-          {{ resort.state }}
+          {{ `${resort.state}, ${resort.country}` }}
           </p>
         </div>
         <div class="col-6 text-end ">
@@ -19,8 +19,17 @@
             <i class="fs-2 mdi mdi-star"></i>
           </div>
         </div>
-        <div class="col-12">
+        <div v-if="resort.resortImg" class="col-12">
          <img class="resort-img img-fluid rounded" :src="resort.resortImg" alt="">
+        </div>
+        <div class="col-3">
+    <PercentageBar :resort="resort" :trailsBar="true" />
+        </div>
+        <div class="col-6">
+    
+        </div>
+        <div class="col-3">
+    
         </div>
       </section>
     </div>
@@ -35,12 +44,25 @@
 <script>
 import { AppState } from '../AppState';
 import { computed, reactive, onMounted } from 'vue';
+import Pop from "../utils/Pop";
+import { resortsService } from "../services/ResortsService";
+import { useRoute } from "vue-router";
+import PercentageBar from "../components/PercentageBar.vue";
 export default {
-  setup(){
-  return { 
-    resort: computed(() => AppState.pinedResort)
-   }
-  }
+    setup() {
+        const route = useRoute();
+        onMounted(() => {
+            getResort();
+        });
+        async function getResort() {
+            const resortId = route.params.resortId;
+            await resortsService.getResortbyId(resortId);
+        }
+        return {
+            resort: computed(() => AppState.pinedResort)
+        };
+    },
+    components: { PercentageBar }
 };
 </script>
 
