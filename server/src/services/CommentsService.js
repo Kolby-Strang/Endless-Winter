8c307f647a5a5ac6c223754b6141b25e40ca5f82
code . <blazej686@yahoo.com>
2023-11-13 11:13:39 -0700

    Likes virtuals done

--- server/src/services/CommentsService.js
+++ server/src/services/CommentsService.js
@@ -9,6 +9,10 @@ class CommentsService {
         }
         comment.body = commentData.body || comment.body
         await comment.save()
+        await comment.populate('account', 'id name picture')
+        await comment.populate('likes')
+        // @ts-ignore
+        comment.likes = comment.likes.map(like => like.accountId)
         return comment
     }
     async getCommentById(commentId) {
@@ -27,11 +31,13 @@ class CommentsService {
         return 'Comment Deleted'
     }
     async getCommentsByThingId(thingId) {
-        const comments = await dbContext.Comment.find({ thingId }).populate('account', 'id name picture')
+        const comments = await dbContext.Comment.find({ thingId }).populate('account', 'id name picture').populate('likes')
+        comments.forEach(comment => comment.likes = comment.likes.map(like => like.accountId))
         return comments
     }
     async createComment(commentData) {
         const comment = await dbContext.Comment.create(commentData)
+        comment.likes = []
         return comment
     }
 
