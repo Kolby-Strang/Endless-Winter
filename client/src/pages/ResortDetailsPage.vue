d0e95281dcffcd16e0042092b9bcbfac0e1775d3
Kolby-Strang <137920922+Kolby-Strang@users.noreply.github.com>
2023-11-14 11:10:39 -0700

    Details Page Progress

--- client/src/pages/ResortDetailsPage.vue
+++ client/src/pages/ResortDetailsPage.vue
@@ -1,31 +1,53 @@
 <template>
     <div class="row page-padding">
-        <div class="col-7">
-            <div class="row">
-                <div class="col-12 pb-3">
+        <div class="col-12 col-md-7">
+            <div class="row gy-3">
+                <div class="col-12">
                     <div class="row bg-blur">
                         <div class="col-7">
                             <h1 class="fw-normal mb-0">{{resort.resortName}}</h1>
-                            <p class="fs-4 fw-light text-primary">{{`${resort.state}, ${resort.country}`}}</p>
+
+                            <p 
+                                title="Click to Copy Full Address" 
+                                role="button" 
+                                class="fs-4 fw-light text-primary"
+                                @click="copyText()"
+                                >
+                                {{`${resort.state}, ${resort.country}`}}<i class="mdi mdi-clipboard-outline"></i>
+                            </p>
+
                             <p class="fs-3 fw-light m-0">{{resort.resortType?.split('_').join(' ')}}</p>
                         </div>
                         <div class="col-5 d-flex justify-content-end align-items-center">
-                            <i class="mdi mdi-check-bold open_close-icon text-success"></i>
+                            <i v-if="resort.resortStatus == 1" class="mdi mdi-check-bold open-icon"></i>
+                            <i v-else-if="resort.resortStatus == 2 || resort.resortStatus == 5 || resort.resortStatus == 6" :title="resort.operatingStatus" class="mdi mdi-clock-outline time-icon"></i>
+                            <i v-else-if="resort.resortStatus == 7" class="mdi mdi-plus-thick closed-icon"></i>
+                            <i v-else-if="resort.resortStatus == 8" class="mdi mdi-white-balance-sunny summer-icon"></i>
+                            <i v-else class="mdi mdi-alert-outline warning-icon" :title="resort.operatingStatus"></i>
                         </div>
                     </div>
                 </div>
 
-                <div class="col-4">
-                    <div class="bg-blur h-100">Trail Img</div>
+                <div class="col-4 ps-0">
+                    <div class="bg-blur h-100">
+                        <div class="position-relative h-100 w-100">
+
+                            <img class="trail-image" :src="resort.trailImg" alt="Trail Map">
+                            
+                            <button class="btn expand-button" type="button" data-bs-toggle="modal" data-bs-target="#trailImageModal">
+                                <i class="mdi mdi-arrow-expand-all fs-1"></i>
+                            </button>
+                        </div>
+                    </div>
                 </div>
 
-                <div class="col-8">
-                    <div class="row">
+                <div class="col-8 pe-0">
+                    <div class="row gy-3">
                         <div class="col-6">
                             <div class="bg-blur h-100">Trail Percent Bars</div>
                         </div>
                         <div class="col-6">
-                            <div class="bg-blur h-100">
+                            <div class="bg-blur h-100" :title="[resort.nightSkiing ? 'Night Skiing Available': 'Day Skiing Only']">
                                 <p class="m-0 d-flex justify-content-around">
                                     <i class="mdi mdi-white-balance-sunny text-warning fs-1"></i>
                                     <i v-if="resort.nightSkiing" class="mdi mdi-moon-waning-crescent moon-color fs-1"></i>
@@ -33,16 +55,29 @@
                                 <p class="m-0 text-center"><i class="mdi mdi-snowboard fs-1 text-primary"></i></p>
                             </div>
                         </div>
-                        <div class="col-12 pt-3">
+                        <div class="col-12">
                             <div class="bg-blur">Snow Conditions</div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-        <div class="col-5">
+        <div class="col-12 col-md-5 p-0 px-md-3">
             <div class="bg-blur h-100">
                 {{ resort }}
+            </div>
+        </div>
+    </div>
+    <div class="modal fade" id="trailImageModal" tabindex="-1" aria-labelledby="trailImageModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-xl">
+            <div class="modal-content bg-blur">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="trailImageModalLabel">Trail Map For {{ resort.resortName }}</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <img class="img-fluid" :src="resort.trailImg" alt="Trail Map Modal">
+                </div>
             </div>
         </div>
     </div>
@@ -55,6 +90,7 @@ import { AppState } from '../AppState';
 import { computed, reactive, onMounted, ref } from 'vue';
 import {resortsService} from '../services/ResortsService'
 import { Resort } from '../models/Resort';
+import Pop from '../utils/Pop';
 export default {
     setup(){
         // VARIABLES
@@ -71,13 +107,21 @@ export default {
             const retrievedResort = await resortsService.getResortById(resortId)
             resort.value = new Resort(retrievedResort)
         }
+        function copyText(){
+            const text = resort.value.address
+            navigator.clipboard.writeText(text)
+            Pop.success('Address Copied!')
+        }
 
         // LIFECYCLE
         onMounted(()=>{
             backgroundImage('src/assets/img/hero.jpg')
             getResortById(route.params.resortId)
         })
-    return { resort }
+    return { 
+        resort,
+        copyText 
+     }
     }
 };
 </script>
@@ -93,11 +137,43 @@ export default {
     padding: 1em;
 }
 
-.open_close-icon{
+.open-icon{
     font-size: 65pt;
+    color: green;
+}
+.closed-icon{
+    font-size: 65pt;
+    color: red;
+    transform: rotate(45deg);
+}
+.summer-icon{
+    font-size: 65pt;
+    color: yellow;
+}
+.time-icon{
+    font-size: 65pt;
+    color: orange;
+}
+.warning-icon{
+    font-size: 65pt;
+    color: orangered;
 }
 .moon-color{
     color: #324279;
+}
+.trail-image{
+    height: 100%;
+    width: 100%;
+    border-radius: 10px;
+    object-fit: cover;
+    object-position: center;
+}
+.expand-button{
+    position: absolute;
+    right: 0;
+    bottom: 0;
+    color: rgb(255, 255, 255);
+    text-shadow: 0 0 20px black;
 }
 
 </style>
