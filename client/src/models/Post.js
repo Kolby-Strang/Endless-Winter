55bc78c262474b420e7ef4508e55b49bb522f556
MarcS2 <marcellosharrai@hotmail.com>
2023-11-14 10:49:28 -0700

    post model

---
+++ client/src/models/Post.js
@@ -0,0 +1,10 @@
+export class Post {
+  constructor(data) {
+    this.id = data.id
+    this.account = data.account
+    this.body = data.body
+    this.snoId = data.snoId
+    this.title = data.title
+  }
+}
+
