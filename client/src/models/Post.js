0343823b3de9d55c89e9c2d16d2d68e0e8ce6200
code . <blazej686@yahoo.com>
2023-11-14 10:34:36 -0700

    Get Post by Id

---
+++ client/src/models/Post.js
@@ -0,0 +1,9 @@
+export class Post {
+    constructor(data) {
+        this.id = data.id
+        this.title = data.title
+        this.body = data.body
+        this.imgUrl = data.imgUrl
+        this.account = data.account
+    }
+}
