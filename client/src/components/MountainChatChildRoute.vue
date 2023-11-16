e81bfffda6642fc2d0a19180a29e7aff46572c86
MarcS2 <marcellosharrai@hotmail.com>
2023-11-16 10:59:21 -0700

    resort page posts

--- client/src/components/MountainChatChildRoute.vue
+++ client/src/components/MountainChatChildRoute.vue
@@ -14,24 +14,26 @@
           <p class="fs-1 mb-0">community chat</p>
         </div>
       </section>
-          <section class="row justify-content-between mt-3">
+          <section class="row justify-content-center mt-3">
             <div class="col-12 col-md-6 col-lg-4 mb-4 rounded px-4 " v-for="post in posts" :key="post.id" >
+              <router-link :to="{name:'Post', params: {postId: post.id}}">
                 <section class="row rounded img-bg linear-bg"
-            :style="{backgroundImage: `url('${post.imgUrl}')`, backgroundPosition: 'center', backgroundSize: 'cover'}">
-            <div class="col-12">
-              <section class="row rounded px-2 ">
-                <div class="col-12 text-light mt-3">
-                  <p v-if="post.imgUrl == undefined" class="mb-0 fs-5">{{ post.body }}</p>
-                </div>
-                <div class="col-1 align-self-end mb-2">
-                  <img class="pfp-img rounded-circle" :src="post.account.picture" :alt="post.account.name">
-                </div>
-                <div class="col-11 text-light align-self-end">
-                  <p class="fs-4 ms-2">{{ post.title }}</p>
+                :style="{backgroundImage: `url('${post.imgUrl}')`, backgroundPosition: 'center', backgroundSize: 'cover'}">
+                <div class="col-12">
+                  <section class="row rounded px-2 linear-bg">
+                    <div class="col-12 text-light mt-3">
+                      <p v-if="post.imgUrl == undefined" class="mb-0 fs-4 ms-1 text-start">{{ post.body }}</p>
+                    </div>
+                    <div class="col-1 col-md-2  align-self-end mb-2">
+                      <img class="pfp-img rounded-circle" :src="post.account.picture" :alt="post.account.name">
+                    </div>
+                    <div class="col-11 col-md-10 text-light align-self-end">
+                      <p class="fs-3 ms-2">{{ post.title }}</p>
+                    </div>
+                  </section>
                 </div>
               </section>
-            </div>
-            </section>
+            </router-link>
             </div>
           </section>
           
@@ -80,7 +82,7 @@ export default {
 
 
 .linear-bg {
-  height: 10dvh;
+  height: 25dvh;
   background-color: #fff;
   background: rgb(0,0,0);
   background: linear-gradient(to bottom,rgba(39,36,84,0.35) 55%, rgba(39,36,84,0.45) 69%, rgba(0,0,0,0.6) 81%, rgba(0,0,0,1) 96%);
