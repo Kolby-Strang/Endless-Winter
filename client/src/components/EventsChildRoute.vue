4df0d06e76acbc83579ea34042c15d0ebdf0fbdf
MarcS2 <marcellosharrai@hotmail.com>
2023-11-16 15:25:05 -0700

    resort page style update

--- client/src/components/EventsChildRoute.vue
+++ client/src/components/EventsChildRoute.vue
@@ -14,9 +14,27 @@
           </router-link>
         </div>
       </section>
-        <div class="col-12">
-          <h1>EVENTS</h1>
+      <section class="row justify-content-center mt-5">
+        <div class="col-11 mb-3">
+          <section class="row img-bg rounded align-content-end" style="background-image: 
+          url(https://media.bogusbasin.org/uploads/2023/05/06114223/FirstChairBannerWithRiders2023-ByLukeTokunaga.jpg);">
+            <div class="col-12">
+              <section class="row justify-content-center">
+                <div class="col-11 blur-bg rounded mb-2 text-light " >
+                  <div class="p-2">
+                    <p class="fs-2">
+                      Lorem ipsum dolor sit amet.
+                    </p>
+                    <p>
+                      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempora iure aliquam nisi dicta at magnam dolore ipsum, quia, sit exercitationem voluptates culpa cum dolorum id, odit repellat animi excepturi incidunt obcaecati molestiae necessitatibus consequuntur iste. Ab doloribus excepturi assumenda aliquam totam vitae. Omnis, deserunt aut velit dolorum ad unde earum sunt esse corporis aperiam culpa blanditiis, illo facilis! Repudiandae molestiae commodi, saepe doloremque facilis nesciunt. Placeat ipsum dicta maxime modi aut! Earum sunt aliquid omnis eos soluta rem laborum qui esse pariatur. Rem quos nostrum molestias. Quis, consequuntur illum! Iure.
+                    </p>
+                  </div>
+                  </div>
+                </section>
+            </div>
+          </section>
         </div>
+      </section>
 </template>
 
 
@@ -34,6 +52,16 @@ export default {
 <style lang="scss" scoped>
 .filter-background {
   background-color: #ffffff75;
+  backdrop-filter: blur(4px);
+}
+
+.img-bg {
+  height: 65dvh;
+}
+
+.blur-bg  {
+  align-content: flex-end;
+  background: #27245465;
   backdrop-filter: blur(4px);
 }
 
